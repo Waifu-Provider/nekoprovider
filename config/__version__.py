df9bcf87f42735502682d936f9375e423a6524e9
Hifumi Sec <56496067+Hifumi-Sec@users.noreply.github.com>
2021-10-09 18:44:21 -0500

    initial commit

---
+++ config/__version__.py
@@ -0,0 +1,7 @@
+#################################################################
+# Neko Provider | v1.0.0                                        #
+# Creator: Hifumi Sec (https://github.com/Hifumi-Sec)           #
+# Open-Source: https://github.com/Waifu-Provider/nekoprovider   #
+#################################################################
+
+VERSION = 'v1.0.0'
